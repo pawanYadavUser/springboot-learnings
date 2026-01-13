a17320590d354ebf5f6c7b5765fd50f23c71cc2b
pawanYadavUser <pawan.yadav@iic.ac.in>
2026-01-14 01:00:33 +0530

    Springboot-interview-questions-25/12/2025

--- src/main/java/com/pawan/hello/helloWorld/HelloController.java
+++ src/main/java/com/pawan/hello/helloWorld/HelloController.java
@@ -1,15 +1,24 @@
 package com.pawan.hello.helloWorld;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+//@Controller
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String helloWorld(){
         return "Hello, world !!!!!!!!!!!!!!";
+    }
+
+    @GetMapping("/controllerAnnotation/")
+    @ResponseBody
+    public String helloWorldFromControllerAnnotation( ){
+        return "Hello world , from controller annotation";
     }
 
     @PostConstruct
