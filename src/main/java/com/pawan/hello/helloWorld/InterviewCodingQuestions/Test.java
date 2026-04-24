99aec1d054fe7db5736f3b55663d539234891ac8
pawanYadavUser <pawan.yadav@iic.ac.in>
2026-04-25 02:32:44 +0530

    Interview_Coding_Practice_25-4-2026

--- src/main/java/com/pawan/hello/helloWorld/InterviewCodingQuestions/Test.java
+++ src/main/java/com/pawan/hello/helloWorld/InterviewCodingQuestions/Test.java
@@ -2,6 +2,8 @@ package com.pawan.hello.helloWorld.InterviewCodingQuestions;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -70,30 +72,65 @@ public class Test {
 //        Supplier → empty cup
 //        Accumulator → adding milk, tea, sugar step by step
 //        Combiner → if 2 people made half chai each → mix them
-        String reversed = IntStream.range(0, input.length())
-                .collect(StringBuilder::new,
-                        (sb, i) -> sb.append(input.charAt(input.length() - 1 - i)),
-                        StringBuilder::append)
-                .toString();
+//        String reversed = IntStream.range(0, input.length())
+//                .collect(StringBuilder::new,
+//                        (sb, i) -> sb.append(input.charAt(input.length() - 1 - i)),
+//                        StringBuilder::append)
+//                .toString();
+//
+//        return input.equals(reversed);
 
+        String reversed = IntStream.range(0,input.length())
+                .collect(StringBuilder::new,(sb,i) -> sb.append(input.charAt(input.length()-i-1)), StringBuilder::append)
+                .toString();
         return input.equals(reversed);
+
+
     }
 
-    public static int findDuplicateInArray(int [] input){
+    public static Set<Integer> findDuplicateInArray(int [] input){
 //        💡 Explanation
 //        HashSet stores seen elements
 //        First repeat = duplicate
 //
 //👉 Time: O(n) | Space: O(n)
         HashSet<Integer> set = new HashSet<>();
-        for(int num : input){
-            if(set.contains(num)) {
-                //return first duplicate!
-                return num;
-            }
-            set.add(num);
-        }
-        return -1;
+//        for(int num : input){
+//            if(set.contains(num)) {
+//                //return first duplicate!
+//                return num;
+//            }
+//            set.add(num);
+//        }
+//        return -1;
+
+//        🔍 Why this is good:
+//        seen.add(n) returns:
+//        true → first time
+//        false → duplicate
+//        So !seen.add(n) filters duplicates
+//        Time complexity → O(n) ✅ (important for interviews)
+//        Set<Integer> duplicates = Arrays.stream(input).filter(n-> !set.add(n)).boxed().collect(Collectors.toSet());
+//        return duplicates;
+
+        // ⚡ Alternative Stream Approach (Grouping)
+//        👍 Pros:
+//        Very readable
+//        Shows knowledge of groupingBy
+//        👎 Cons:
+//        Extra space
+//        Slightly heavier than needed
+
+
+//        ❌ What NOT to do (bad interview answer)
+//        list.stream()
+//                .filter(x -> Collections.frequency(list, x) > 1)
+//
+//        👉 This is O(n²) → interviewers will reject it.
+        Map<Integer, Long> frequency = Arrays.stream(input).boxed().collect(Collectors.groupingBy(n-> n, Collectors.counting()));
+        Set <Integer> duplicates = frequency.entrySet().stream().filter(e->e.getValue()>1).map(Map.Entry::getKey).collect(Collectors.toSet());
+        return duplicates;
+
     }
 
 
@@ -132,15 +169,15 @@ public class Test {
     }
 
     public static void main(String[] args) {
-        String str = "helloWorld";
-         int[] arr1 = {1,2,2,3,4,5,6,7,8,9};
+        String str = "helloolleh";
+         int[] arr1 = {1,2,2,3,4,5,6,7,7,8,8,9};
          int [] arr2 = {0,1,0,2,3,0,0,4,0,5,0,6,0,9,9};
 
         System.out.println("reversed string : "+reverseString(str));
         System.out.println("Is palindrome : "+str+" | "+isPalindrome(str));
         System.out.println("Is palindrome : "+"4994"+" | "+isPalindrome("4994"));
 
-        System.out.println("Find duplicate in array: "+ Arrays.toString(arr1)+ " | "+findDuplicateInArray(arr1));
+        System.out.println("Find duplicate in array: "+ Arrays.toString(arr1)+ " | "+findDuplicateInArray(arr1).toString());
 
         System.out.println("Move zeros to end for arr : "+Arrays.toString(arr2) + " | "+Arrays.toString(moveZeroesToEnd(arr2)));
 
